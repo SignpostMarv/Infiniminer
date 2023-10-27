a68724745155358810607fc8861fdad2d7ac4289
Christopher Whitley <whitleych17@ecu.edu>
2023-10-27 16:26:05 -0400

    Gamepad to GamePad

--- source/Infiniminer/Infiniminer.Server/InfiniminerServer.cs
+++ source/Infiniminer/Infiniminer.Server/InfiniminerServer.cs
@@ -2461,7 +2461,7 @@ namespace Infiniminer
         public void VibrateGamePad(Player player, float strength, uint ms)
         {
             NetBuffer? msgBuffer = netServer?.CreateBuffer();
-            msgBuffer?.Write((byte)InfiniminerMessage.VibrateGamepad);
+            msgBuffer?.Write((byte)InfiniminerMessage.VibrateGamePad);
             msgBuffer?.Write(strength);
             msgBuffer?.Write(ms);
             netServer?.SendMessage(msgBuffer, player.NetConn, NetChannel.ReliableInOrder1);
