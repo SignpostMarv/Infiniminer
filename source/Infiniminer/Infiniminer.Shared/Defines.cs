0cbdeb769c7e30ff9135642acd689ca5645fb79a
Christopher Whitley <whitleych17@ecu.edu>
2023-10-27 16:33:43 -0400

    Bump version number since server/client not compatible with 1.5

--- source/Infiniminer/Infiniminer.Shared/Defines.cs
+++ source/Infiniminer/Infiniminer.Shared/Defines.cs
@@ -29,7 +29,7 @@ namespace Infiniminer;
 
 public class Defines
 {
-    public const string INFINIMINER_VERSION = "v1.5";
+    public const string INFINIMINER_VERSION = "v1.6";
     public const int GROUND_LEVEL = 8;
 
     /*
