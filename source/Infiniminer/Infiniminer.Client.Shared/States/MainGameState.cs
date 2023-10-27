05df188b2efd59a9d9f530631271798e01179dec
Christopher Whitley <whitleych17@ecu.edu>
2023-10-27 14:58:26 -0400

    Moved block type switch to main update, added controller support

--- source/Infiniminer/Infiniminer.Client.Shared/States/MainGameState.cs
+++ source/Infiniminer/Infiniminer.Client.Shared/States/MainGameState.cs
@@ -223,6 +223,20 @@ namespace Infiniminer.States
                     }
                 }
 
+                ///////////////////////////////////////////////////////////////////
+                /// Check if the player is using the construction gun and if they
+                /// want to switch block types
+                ///////////////////////////////////////////////////////////////////
+                if (_P.playerTools[_P.playerToolSelected] == PlayerTools.ConstructionGun && _P.inputEngine.ChangeBlockType.Pressed())
+                {
+                    _P.PlaySound(InfiniminerSound.ClickLow);
+                    _P.playerBlockSelected += 1;
+                    if (_P.playerBlockSelected >= _P.playerBlocks.Length)
+                    {
+                        _P.playerBlockSelected = 0;
+                    }
+                }
+
                 // Update the player's position.
                 UpdatePlayerPosition(gameTime, keyState);
                 
@@ -535,27 +549,6 @@ namespace Infiniminer.States
 
             if (!_P.playerDead)
             {
-                // Change block type!
-                if (key == Keys.R && _P.playerTools[_P.playerToolSelected] == PlayerTools.ConstructionGun)
-                {
-                    _P.PlaySound(InfiniminerSound.ClickLow);
-                    _P.playerBlockSelected += 1;
-                    if (_P.playerBlockSelected >= _P.playerBlocks.Length)
-                        _P.playerBlockSelected = 0;
-                }
-                /* Ported from HandleInput but not in use here
-                else if (
-                    false &&
-                    _P.playerTools[_P.playerToolSelected] == PlayerTools.ConstructionGun
-                )
-                {
-                    _P.PlaySound(InfiniminerSound.ClickLow);
-                    _P.playerBlockSelected -= 1;
-                    if (_P.playerBlockSelected < 0)
-                        _P.playerBlockSelected = _P.playerBlocks.Length-1;
-                }
-                */
-
                 // Deposit and withdraw from a bank.
                 if (_P.AtBankTerminal())
                 {
