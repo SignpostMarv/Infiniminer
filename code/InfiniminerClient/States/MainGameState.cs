22a53f63411deee4d1c9581c765ab6d4dc879070
SignpostMarv <me@signpostmarv.name>
2026-08-21 22:41:22 +0000

    removing unused using for Storage

--- code/InfiniminerClient/States/MainGameState.cs
+++ code/InfiniminerClient/States/MainGameState.cs
@@ -12,7 +12,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Net;
-using Microsoft.Xna.Framework.Storage;
 using Lidgren.Network;
 
 namespace Infiniminer.States
