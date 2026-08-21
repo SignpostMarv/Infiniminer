7f18ff4e1eb3ee324844f2cb3bc4db3be1e49e8e
SignpostMarv <me@signpostmarv.name>
2026-08-21 23:13:27 +0000

    remove unused using for Graphics

--- code/InfiniminerClient/States/DebugState.cs
+++ code/InfiniminerClient/States/DebugState.cs
@@ -8,7 +8,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.GamerServices;
-using Microsoft.Xna.Framework.Graphics;
 
 namespace Infiniminer.States
 {
