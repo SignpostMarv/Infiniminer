9cb91fdb066f5c117c924af0300ffcec91b8c61f
SignpostMarv <me@signpostmarv.name>
2026-08-22 00:19:13 +0000

    removing unused using for Design

--- code/InfiniminerClient/InterfaceItems/InterfaceButtonToggle.cs
+++ code/InfiniminerClient/InterfaceItems/InterfaceButtonToggle.cs
@@ -6,7 +6,6 @@ using System.Diagnostics;
 using StateMasher;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Design;
 using Infiniminer;
 
 namespace InterfaceItems
